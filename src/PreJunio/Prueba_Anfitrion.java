f2e7e6779eb31b2f1e1799151563ea88bce5747c
user <user@Eclipse>
2024-06-06 09:12:27 +0200

    Hola gente

---
+++ src/PreJunio/Prueba_Anfitrion.java
@@ -0,0 +1,13 @@
+package PreJunio;
+
+public class Prueba_Anfitrion {
+	
+	public static void main(String[] args) {
+			
+			System.out.println("Hola mundo");
+			
+			System.out.println("Buenas noches, mundo");
+			
+		}
+
+}
